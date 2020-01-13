5c3f2533ae85806441213118db7ca0f7ebcab7fd
hongyanyang1 <hongyanyang1@creditease.cn>
2020-01-13 15:17:45 +0800

    增加一些标记使用的注解

---
+++ high-concurrence/src/main/java/com/study/repository/high/concurrence/annoations/NotThreadSafe.java
@@ -0,0 +1,1 @@
+package com.study.repository.high.concurrence.annoations;import java.lang.annotation.ElementType;import java.lang.annotation.Retention;import java.lang.annotation.RetentionPolicy;import java.lang.annotation.Target;/** * 线程不安全 */@Target(ElementType.TYPE)@Retention(RetentionPolicy.SOURCE)public @interface NotThreadSafe {    String value() default "";}
